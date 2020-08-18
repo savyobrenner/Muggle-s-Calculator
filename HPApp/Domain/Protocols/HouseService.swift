8818808a0f7a0c498b330a334e0fbebeed069cf6
Brenner <savyobrenner@gmailcom>
2020-08-18 00:11:45 -0300

    fix houseService

--- HPApp/Domain/Protocols/HouseService.swift
+++ HPApp/Domain/Protocols/HouseService.swift
@@ -1,8 +1,5 @@
 protocol HouseService {
-    
-    func getRandomHouse(onSuccess: @escaping (String) -> Void, onError: @escaping (DataError) -> Void)
-    
+        
     func getAllHouses(onSuccess: @escaping ([Houses]) -> Void, onError: @escaping (DataError) -> Void)
     
-    func getSpecifHouse(houseID: String,onSuccess: @escaping (Houses) -> Void, onError: @escaping (DataError) -> Void)
 }
--- HPApp/Infra/AlamofireHousesService.swift
+++ HPApp/Infra/AlamofireHousesService.swift
@@ -16,20 +16,5 @@ class AlamofireHousesService: HouseService {
         }
     }
     
-    func getRandomHouse(onSuccess: @escaping (String) -> Void, onError: @escaping (DataError) -> Void) {
-        client.create(type: String.self, endPoint: "sortingHat}", onSuccess: { (house) in
-          onSuccess(house)
-        }) { (error) in
-            onError(error.self)
-        }
-      }
-      
-      func getSpecifHouse(houseID: String, onSuccess: @escaping (Houses) -> Void, onError: @escaping (DataError) -> Void) {
-          client.create(type: Houses.self, endPoint: "houses/{\(houseID)}", onSuccess: { (house) in
-            onSuccess(house)
-          }) { (error) in
-              onError(error.self)
-          }
-      }
     
 }
